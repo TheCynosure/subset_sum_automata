d98e8e54e591433d25eb8063ab9aca55e52c10d2
TheCynosure <jack.github@gmail.com>
2017-08-25 22:25:34 -0400

    Idk why there was a git folder in here

---
+++ subset_problem/target/debug/deps/subset_problem-880769ac08aaf32a.d
@@ -0,0 +1,5 @@
+/home/jack/Dev/Projects/subset_sum_automata/subset_problem/target/debug/deps/subset_problem-880769ac08aaf32a: main.rs
+
+/home/jack/Dev/Projects/subset_sum_automata/subset_problem/target/debug/deps/subset_problem-880769ac08aaf32a.d: main.rs
+
+main.rs:
